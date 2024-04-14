4fb3bf19a3a718cb9e1a8ba1dac99461cf2f10be
joaolfp <joaolucasfp2001@gmail.com>
2024-04-13 21:25:12 -0300

    Created code to download video and transcribe

--- py_tube_transcribe/__main__.py
+++ py_tube_transcribe/__main__.py
@@ -2,4 +2,12 @@ from py_tube_transcribe.transcribe import Transcribe
 
 if __name__ == "__main__":
     transcribe = Transcribe()
-    transcribe.run_app()
+    video_url = input("Enter the YouTube video link: ").split('&')[0]
+    video_file = transcribe.download_video(video_url)
+
+    if video_file:
+        audio_file = transcribe.extract_audio(video_file)
+        if audio_file:
+            transcribe.transcribe_audio(audio_file)
+    else:
+        print("The video could not be downloaded.")
--- py_tube_transcribe/transcribe.py
+++ py_tube_transcribe/transcribe.py
@@ -1,4 +1,59 @@
-class Transcribe(object):
+import os
+import ssl
+import subprocess
+import speech_recognition as sr
+from pytube import YouTube
 
-    def run_app(self):
-        print("Welcome PyTubeTranscribe")
+class Transcribe:
+    def __init__(self):
+        pass
+    
+    def download_video(self, url):
+        try:
+            ssl._create_default_https_context = ssl._create_unverified_context
+            
+            yt = YouTube(url)
+            stream = yt.streams.get_highest_resolution()
+            output_path = os.path.join(os.getcwd(), "videos")
+            if not os.path.exists(output_path):
+                os.makedirs(output_path)
+
+            video_file = os.path.join(output_path, stream.default_filename)
+            stream.download(output_path)
+            print("Download completed")
+            return video_file
+        except Exception as e:
+            print(f"An error occurred during the download: {e}")
+            return None
+
+    def extract_audio(self, video_file):
+        try:
+            audio_file = video_file[:-4] + ".wav"
+
+            command = ["ffmpeg", "-i", video_file, "-vn", "-acodec", "pcm_s16le", "-ar", "44100", "-ac", "2", audio_file]
+            subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+
+            print("Extract full audio!")
+            return audio_file
+        except Exception as e:
+            print(f"An error occurred while extracting the audio: {e}")
+            return None
+
+    def transcribe_audio(self, audio_file):
+        try:
+            if os.path.exists(audio_file):
+                recognizer = sr.Recognizer()
+
+                with sr.AudioFile(audio_file) as source:
+                    audio_data = recognizer.record(source)
+
+                text = recognizer.recognize_google(audio_data, language='pt-BR')
+                print("Transcription:")
+                print(text)
+                return text
+            else:
+                print("Audio file not found")
+                return None
+        except Exception as e:
+            print(f"An error occurred during transcription: {e}")
+            return None
